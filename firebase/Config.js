e1e31e43f7e4985403e5a1e15d43d20d9d4ea29f
Pekka Ojala <pojala@oamk.fi>
2023-02-23 09:50:25 +0200

    Initial commit

---
+++ firebase/Config.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  initializeAuth, 
+  getReactNativePersistence } from 'firebase/auth/react-native';
+
+const firebaseConfig = ({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+});
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+export { auth };
+
+export const TODOS_REF = '/todos/';
+export const USERS_REF = '/users/';
